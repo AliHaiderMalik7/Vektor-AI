7c0d9b3eeabf0a5567efb7212625822db582b599
Muhammad Abdullah <ramzanabdullah048@gmail.com>
2025-10-15 05:25:50 -0700

    Minor Changes in start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -e
 echo "🚀 Starting FastAPI app on Railway..."
 # Install dependencies
 pip install -r requirements.txt
 # Starting FastAPI application
+PORT=${PORT:-8000}
 uvicorn app.main:app --host 0.0.0.0 --port $PORT
