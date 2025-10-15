19ac644022da6665d9108f10521216755c99c01a
AliHaiderMalik7 <alihaidermalik777@gmail.com>
2025-10-15 12:49:31 +0500

    changes on home page

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -44,6 +44,7 @@ function Home() {
              model: "gpt-4.1-mini",
              system_message: "You are a helpful travel assistant.",
              enable_web_search: true,
+             conversation_id:1
            }),
          }
        );
