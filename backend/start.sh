99e15324bbbb20ccd14125c04e33486d69045721
Muhammad Abdullah <ramzanabdullah048@gmail.com>
2025-10-15 07:27:59 -0700

    fix errors

--- backend/start.sh
+++ backend/start.sh
@@ -2,8 +2,10 @@
 set -e
 echo "🚀 Starting FastAPI app..."
 
+# Install heavy packages at runtime
+pip install --no-cache-dir torch opencv-python
+
 # Use Railway's port if available, otherwise 8000
 PORT=${PORT:-8000}
-# echo "Using PORT=$PORT"
 
 uvicorn app.main:app --host 0.0.0.0 --port $PORT
