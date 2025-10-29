acc2ee9f417b505f83a7093f22ac6f0c520969d1
Muhammad Abdullah <ramzanabdullah048@gmail.com>
2025-10-29 07:38:18 -0700

    added more gifs

--- backend/app/schema/exercise_media_map.py
+++ backend/app/schema/exercise_media_map.py
@@ -3,8 +3,48 @@ EXERCISE_MEDIA_MAP = {
         "gif": "/static/gifs/pushups.gif"
     },
 
+    "archer-pushup": {
+        "gif": "/static/gifs/archer_pushup.gif"
+    },
+
+    "clap-pushup": {
+        "gif": "/static/gifs/clap_pushup.gif"
+    },
+
+    "close-grip-pushup": {
+        "gif": "/static/gifs/close_grip_pushup.gif"
+    },
+
+    "hand-stand-pushup": {
+        "gif": "/static/gifs/hand_stand_pushup.gif"
+    },
+
+    "hindu-pushup": {
+        "gif": "/static/gifs/hindu_pushup.gif"
+    },
+
+    "incline-pushup": {
+        "gif": "/static/gifs/incline_pushup.gif"
+    },
+
+    "pike-pushup": {
+        "gif": "/static/gifs/pike_pushup.gif"
+    },
+
+    "weighted-pushup": {
+        "gif": "/static/gifs/weighted_pushup.gif"
+    },
+
+    "heel-touch": {
+        "gif": "/static/gifs/heel_touch.gif"
+    },
+
     "plank": {
         "gif": "/static/gifs/plank.gif"
+    },
+
+    "side-plank": {
+        "gif": "/static/gifs/side_plank.gif"
     },
 
     "squats": {
@@ -53,6 +93,22 @@ EXERCISE_MEDIA_MAP = {
 
     "climbing-stairs":{
         "gif": "/static/gifs/climbing_stairs.gif"
+    },
+
+    "leg-raise":{
+        "gif": "/static/gifs/leg_raise.gif"
+    },
+
+    "calf-raise":{
+        "gif": "/static/gifs/calf_raise.gif"
+    },
+
+    "mountain-climber":{
+        "gif": "/static/gifs/mountain_climber.gif"
+    },
+
+    "high-knee":{
+        "gif": "/static/gifs/high_knee.gif"
     },
 
 }
